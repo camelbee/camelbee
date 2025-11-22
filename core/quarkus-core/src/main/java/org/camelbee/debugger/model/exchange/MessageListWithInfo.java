210a23a88b381deae5bef543d1089e0edfdcd18e
ekaraosmanoglu <ege.karaosmanoglu@vodafoneziggo.com>
2025-11-22 18:41:18 +0100

    chore: quarkus native compilation dependencies added.

--- core/quarkus-core/src/main/java/org/camelbee/debugger/model/exchange/MessageListWithInfo.java
+++ core/quarkus-core/src/main/java/org/camelbee/debugger/model/exchange/MessageListWithInfo.java
@@ -1,10 +1,12 @@
 package org.camelbee.debugger.model.exchange;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import java.util.List;
 
 /**
  * Enhanced MessageList that includes version information.
  */
+@RegisterForReflection
 public class MessageListWithInfo {
 
   private final List<Message> messages;
